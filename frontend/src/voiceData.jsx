27941e652c836579d63e212775e12c83cee040a3
MAHESH AKARAMSETTY <akaramsettymahesh4800@gmail.com>
2025-08-10 19:21:17 +0530

    Create voiceData.jsx

---
+++ frontend/src/voiceData.jsx
@@ -0,0 +1,92 @@
+export const englishVoiceData = {
+  neural: {
+    available_languages: [
+      "English (US)",
+      "English (British)",
+      "English (Australian)",
+      "English (Indian)",
+      "English (Irish)",
+      "English (New Zealand)",
+      "English (Singaporean)",
+      "English (South African)"
+    ],
+    voices: {
+      "English (US)": [
+        { name: "Joanna", gender: "Female" },
+        { name: "Salli", gender: "Female" },
+        { name: "Matthew", gender: "Male" },
+        { name: "Kimberly", gender: "Female" },
+        { name: "Kendra", gender: "Female" },
+        { name: "Justin", gender: "Male" },
+        { name: "Joey", gender: "Male" },
+        { name: "Ivy", gender: "Female" },
+        { name: "Danielle", gender: "Female" },
+        { name: "Gregory", gender: "Male" },
+        { name: "Ruth", gender: "Female" },
+        { name: "Kevin", gender: "Male" },
+        { name: "Stephen", gender: "Male" }
+      ],
+      "English (British)": [
+        { name: "Emma", gender: "Female" },
+        { name: "Brian", gender: "Male" },
+        { name: "Amy", gender: "Female" },
+        { name: "Arthur", gender: "Male" }
+      ],
+      "English (Australian)": [
+        { name: "Olivia", gender: "Female" }
+      ],
+      "English (Indian)": [
+        { name: "Kajal", gender: "Female" }
+      ],
+      "English (Irish)": [
+        { name: "Niamh", gender: "Female" }
+      ],
+      "English (New Zealand)": [
+        { name: "Aria", gender: "Female" }
+      ],
+      "English (Singaporean)": [
+        { name: "Jasmine", gender: "Female" }
+      ],
+      "English (South African)": [
+        { name: "Ayanda", gender: "Female" }
+      ]
+    }
+  },
+  standard: {
+    available_languages: [
+      "English (US)",
+      "English (British)",
+      "English (Australian)",
+      "English (Indian)",
+      "English (Welsh)"
+    ],
+    voices: {
+      "English (US)": [
+        { name: "Joanna", gender: "Female" },
+        { name: "Salli", gender: "Female" },
+        { name: "Matthew", gender: "Male" },
+        { name: "Kimberly", gender: "Female" },
+        { name: "Kendra", gender: "Female" },
+        { name: "Justin", gender: "Male" },
+        { name: "Joey", gender: "Male" },
+        { name: "Ivy", gender: "Female" }
+      ],
+      "English (British)": [
+        { name: "Emma", gender: "Female" },
+        { name: "Brian", gender: "Male" },
+        { name: "Amy", gender: "Female" }
+      ],
+      "English (Australian)": [
+        { name: "Russell", gender: "Male" },
+        { name: "Nicole", gender: "Female" }
+      ],
+      "English (Indian)": [
+        { name: "Raveena", gender: "Female" },
+        { name: "Aditi", gender: "Female" }
+      ],
+      "English (Welsh)": [
+        { name: "Geraint", gender: "Male" }
+      ]
+    }
+  }
+};
